5c582afc84b17f8a723e8f106dc99704fa362752
C <cj6209577@sina.com>
2022-09-13 11:04:11 +0800

    feat: 修改eslint的报错规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: C.
+ * @Date: 2022-09-13 10:45:20
+ * @LastEditTime: 2022-09-13 11:03:49
+ * @Description: file content
+ */
 module.exports = {
   env: {
     browser: true,
@@ -18,10 +24,16 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: ['vue', '@typescript-eslint'],
+  globals: {
+    // 不希望被eslint报错的全局变量
+    AMap: true
+  },
   rules: {
     // "off" or 0 - 关闭规则
     // "warn" or 1 - 将规则视为一个警告
     // "error" or 2 - 将规则视为一个错误
-    eqeqeq: 2 // 强制使用 === 和 !==
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': 1
   }
 }
