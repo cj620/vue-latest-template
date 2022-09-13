3b057b4a85dd2618a9890fd4df8d5eabb1cd4740
C <cj6209577@sina.com>
2022-09-13 10:20:01 +0800

    test: commitlint 提交测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: C.
  * @Date: 2022-09-13 09:00:14
- * @LastEditTime: 2022-09-13 10:02:05
+ * @LastEditTime: 2022-09-13 10:18:19
  * @Description: file content
 -->
 <script setup lang="ts">
@@ -13,7 +13,7 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}test</h1>
+  <h1>{{ msg }}</h1>
 
   <p>
     Recommended IDE setup:
