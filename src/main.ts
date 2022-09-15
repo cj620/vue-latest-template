8024111e68b6f9a7c44d13c1844313e2e458728c
C <cj6209577@sina.com>
2022-09-15 09:10:14 +0800

    feat: add CSS resets

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
+/*
+ * @Author: C.
+ * @Date: 2022-09-15 09:02:10
+ * @LastEditTime: 2022-09-15 09:09:46
+ * @Description: file content
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
 createApp(App).mount('#app')
